3e756c0981070fd6801defa7b384aeba2820cab1
Álvaro Noguer <alvaronoguer@gmail.com>
2022-07-09 09:30:44 +0200

    Labdone

--- js/index.js
+++ js/index.js
@@ -2,10 +2,10 @@
 //
 console.log("I'm ready!");
 
-let hacker1 = "Andriu"; 
+let hacker1 = "Acvaro"; 
 console.log("The driver's name is " +hacker1);
 
-let hacker2 = "Marrket"; 
+let hacker2 = "Antonius"; 
 console.log("The navigator's name is " +hacker2)
 
 // Iteration 2: Conditionals
@@ -32,6 +32,39 @@ for (let i = 1; i < name.length; i++) {
 
 console.log(name2)
 
+let nameReverse = '';
+let name3 = hacker1.toString();
+
+for (let i = name3.length -1; i >= 0; i--) {
+    nameReverse = nameReverse + name3[i];
+}
+
+console.log(nameReverse)
 
 
+// 
 
+let nameHa = hacker1.toLowerCase();
+let nameHb = hacker2.toLowerCase();
+let contA = 0;
+let contB = 0;
+
+
+for (let i = 0; i < nameHa.length; i++) {
+    if (nameHa[i] > nameHb[i]) {
+        contA++
+    } else if (nameHa[i] < nameHb[i]) {
+        contB++
+    }
+}
+
+console.log(contA);
+console.log(contB);
+
+if (contA > contB) {
+    console.log(`The driver's name goes first.`)
+} else if (contA < contB) {
+    console.log(`Yo, the navigator goes first definitely`)
+} else {
+    console.log(`What?! You both have the same name?`)
+}
